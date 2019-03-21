3f913e403f73b3f358589bfbcefb990e11c0b8d8
xion <313183373@qq.com>
2019-03-21 16:04:34 +0800

    change language name c++ to cpp

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     secret: process.env.SECRET,
     tokenHeader: 'x-access-token',
-    supportLanguage: ['c', 'c++'],
+    supportLanguage: ['c', 'cpp'],
 };
