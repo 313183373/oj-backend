ed4bf08dde8c7530c848ed313046413eab35f3db
xion <313183373@qq.com>
2019-03-26 01:45:40 +0800

    socket test

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -10,10 +10,14 @@ module.exports = {
     console.log('a client connected');
 
     socket.on('listenToSubmit', function (submitId) {
+      console.log('get a listenToSubmit event from client, submitId' + submitId);
       socketEmitter.on('judgeEnd', submit => {
-        console.log('receive judgeEnd', submit);
+        console.log('receive judgeEnd', submit._id);
         if (submit._id === submitId) {
+          console.log('send back the result to client');
           socket.emit('result', JSON.stringify(submit));
+        } else {
+          console.log('submit id is not right');
         }
       })
     });
