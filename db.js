74a31c272214215aa90aacc197e08a23a61498a4
xion <313183373@qq.com>
2019-04-21 16:01:11 +0800

    reconnect to mongodb

--- db.js
+++ db.js
@@ -2,24 +2,27 @@ const mongoose = require('mongoose');
 const isProduction = process.env.NODE_ENV === 'production';
 mongoose.promise = global.Promise;
 
-if(isProduction) {
-    mongoose.connect('mongodb://db:27017/oj', {
-        useNewUrlParser: true
-    });
-} else {
-    mongoose.connect('mongodb://localhost:27017/oj', {
-        useNewUrlParser: true
-    });
+const mongodbUrl = isProduction ? 'mongodb://db:27017/oj' : 'mongodb://localhost:27017/oj';
 
+function connectWithRetry() {
+  mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+  });
 }
+
+connectWithRetry();
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', () => {
+  console.log('connect to mongodb failed');
+  setTimeout(connectWithRetry, 500);
+});
+
 db.once('open', function () {
-    console.log('mongodb connected!');
+  console.log('mongodb connected!');
 });
 
 if (!isProduction) {
-    mongoose.set('debug', true);
+  mongoose.set('debug', true);
 }
